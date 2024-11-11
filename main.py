0f050c59107ae699389ccf36405442a14cde572a
lz9848 <74493657+lz9848@users.noreply.github.com>
2024-11-11 12:44:11 +0800

    update

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from sb3_contrib import RecurrentPPO
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 import numpy as np
 import gymnasium as gym
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.monitor import Monitor
 from env import AvdEnvVecObs
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.utils import get_linear_fn
 from model import CustomActorCriticPolicy
 import os
 
@@ -13,14 +14,15 @@ gamma = 0.95
 gae_lambda = 0.95
 rollout_len = 512
 parallel_num = 8
-iterations = 5000
+iterations = 10000
 features_dim = 512
-exp_name = f"PPO-{features_dim}-{gamma}-{gae_lambda}"
+lr_schedule = get_linear_fn(0.0001, 0.00005, 0.05)
+exp_name = f"PPOv2-test-{features_dim}-{gamma}-{gae_lambda}"
 
 
 def make_env():
     env = AvdEnvVecObs()
-    env = Monitor(env)  # 包裹在 Monitor 中，用于记录奖励和回合长度
+    env = Monitor(env)
     return env
 
 
@@ -44,7 +46,7 @@ class Callback_PPO(BaseCallback):
     def _on_rollout_end(self):
         if self.model.num_timesteps // self.reboot_interval > self.reboot_count:
             print("###save model###")
-            self.model.save(f"{exp_name}-{int(self.model.num_timesteps//1e6)}M")
+            self.model.save(f"{exp_name}-{int(self.model.num_timesteps // 1e6)}M")
             print("###reboot env###")
             self.reboot_count += 1
             self.training_env.close()
@@ -53,14 +55,15 @@ class Callback_PPO(BaseCallback):
 
 def train_avd_PPO():
     policy_kwargs = dict(
-        net_arch=[512, 256, 256],
+        net_arch=[features_dim, 256, 256],
         features_dim=features_dim,
-        model_dim=128,
+        d_k=128,
+        n_head=4
     )
 
     custom_hyperparams = {
         "policy_kwargs": policy_kwargs,
-        "learning_rate": 0.0001,
+        "learning_rate": lr_schedule,
         "n_steps": rollout_len,
         "batch_size": 512,
         "n_epochs": 3,
@@ -69,19 +72,17 @@ def train_avd_PPO():
         "clip_range": 0.2,
         "ent_coef": 0.01,
         "vf_coef": 1.0,
-        "max_grad_norm": 5.0,
+        "max_grad_norm": 7.5,
         "tensorboard_log": f"./log/avoidance/{exp_name}",
         "verbose": 1,
     }
 
     vec_env = SubprocVecEnv([make_env for _ in range(parallel_num)])
-    if os.path.exists("model.zip"):
-        model = PPO.load("eval", env=vec_env, print_system_info=True)
+    if os.path.exists("ppo.zip"):
+        model = PPO.load("ppo", env=vec_env, print_system_info=True)
+        model.learning_rate = 0.00005
     else:
         model = PPO(CustomActorCriticPolicy, vec_env, **custom_hyperparams)
-
-    total_params = sum(p.numel() for p in model.policy.parameters())
-    print(f"模型参数总数: {total_params}")
 
     callback = Callback_PPO(verbose=1)
     callback.init_callback(model)
@@ -104,7 +105,7 @@ def train_atari():
 
     custom_hyperparams = {
         "learning_rate": 0.0001,
-        "n_steps": rollout_len//parallel_num,
+        "n_steps": rollout_len // parallel_num,
         "batch_size": 128,
         "n_epochs": 5,
         "gamma": 0.99,
@@ -115,7 +116,7 @@ def train_atari():
     }
 
     model = RecurrentPPO("CnnLstmPolicy", vec_env, **custom_hyperparams)
-    model.learn(total_timesteps=(rollout_len*1000))
+    model.learn(total_timesteps=(rollout_len * 1000))
 
 
 if __name__ == '__main__':
